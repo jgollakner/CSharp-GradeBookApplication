23af64fd8459f3cc23ded811952b5b3defd97b09
jgollakner <joegollakner@me.com>
2019-04-18 12:33:44 -0500

    Update StartingUserInterface.cs

--- GradeBook/UserInterfaces/StartingUserInterface.cs
+++ GradeBook/UserInterfaces/StartingUserInterface.cs
@@ -40,18 +40,19 @@ namespace GradeBook.UserInterfaces
                 return;
             }
             var name = parts[2];
-            BaseGradeBook gradeBook = null;
-            switch (name)
+            BaseGradeBook gradeBook;
+            if (name == "standard")
             {
-                case "standard":
-                    gradeBook = new StandardGradeBook(name);
-                    break;
-                case "ranked":
-                    gradeBook = new RankedGradeBook(name);
-                    break;
-                default:
-                    Console.WriteLine($"{name} is not a supported type of gradebook, please try again.");
-                break;
+                gradeBook = new StandardGradeBook(name);
+            }
+            else if (name == "ranked")
+            {
+                gradeBook = new RankedGradeBook(name);
+            }
+            else
+            {
+                Console.WriteLine($"{name} is not a supported type of gradebook, please try again.");
+                return;
             }
 
             Console.WriteLine("Created gradebook {0}.", name);
